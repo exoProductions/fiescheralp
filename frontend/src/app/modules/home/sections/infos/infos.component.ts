f5cd1be2de83fa20a6dffdfa5a1d4d566c384ee9
dominic gut <gut.dominic@gmx.ch>
2022-11-13 15:32:22 +0100

    updated infos component home

--- frontend/src/app/modules/home/sections/infos/infos.component.ts
+++ frontend/src/app/modules/home/sections/infos/infos.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfosComponent implements OnInit {
 
+  infos: any[] = [
+    {
+      title: "Anreise",
+      info: "Für die Anreise stehen Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      icon: "trail-sign-outline",
+    },
+    {
+      title: "Hausregeln",
+      info: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      icon: "book-outline",
+    },
+    {
+      title: "Ortsinfo",
+      info: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      icon: "location-outline",
+    },
+  ]
+  openInd: number = -1;
+
   constructor() { }
 
   ngOnInit(): void {
