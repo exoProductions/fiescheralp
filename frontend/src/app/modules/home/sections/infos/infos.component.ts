ce1f2aa535c4027ac32eab6121999486f3f4f606
dominic gut <gut.dominic@gmx.ch>
2022-11-14 09:49:18 +0100

    info maps update and review style debugged

--- frontend/src/app/modules/home/sections/infos/infos.component.ts
+++ frontend/src/app/modules/home/sections/infos/infos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-infos',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infos.component.less']
 })
 export class InfosComponent implements OnInit {
+
+  @ViewChild("info") infoElem!:ElementRef;
 
   infos: any[] = [
     {
@@ -24,12 +27,12 @@ export class InfosComponent implements OnInit {
       icon: "location-outline",
     },
   ];
-  zoom = 1;
+  zoom = 10;
 //  center: google.maps.LatLngLiteral = { lat: 46.40147919848464, lng: 8.131963355266702 };
 
   center: any = { lat: 46.40147919848464, lng: 8.131963355266702 };
   options: any = { // google.maps.MapOptions
-    mapTypeId: 'hybrid',
+    mapTypeId: 'roadmap',
     zoomControl: true,
     scrollwheel: false,
     disableDoubleClickZoom: true,
@@ -38,15 +41,14 @@ export class InfosComponent implements OnInit {
   };
 
   markers: any[] = [];
-
+  markerLinks:string[]=[
+    "https://www.google.ch/maps/search/fiescheralp/@46.4051579,8.0710231,12.02z",
+  ];
   openInd: number = -1;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
-    
-
-
     this.markers.push({
       position: {
         lat: this.center.lat,
@@ -54,6 +56,18 @@ export class InfosComponent implements OnInit {
       },
       //options: { animation: google.maps.Animation.BOUNCE },
     });
+  }
+
+  openMaps(ind:number):void{
+    console.log("clicked");
+    window.open(this.markerLinks[ind],"_blank");
+  }
+
+  openInfo(ind:number):void{
+    this.openInd=ind;
+    setTimeout(()=>{
+      this.infoElem.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    },100);
   }
 
 }
