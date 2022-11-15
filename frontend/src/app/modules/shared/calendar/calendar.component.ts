29afc902f5d12162380171d1d53ea901984464ca
dominic gut <gut.dominic@gmx.ch>
2022-11-15 20:14:58 +0100

    calendar functionality update

--- frontend/src/app/modules/shared/calendar/calendar.component.ts
+++ frontend/src/app/modules/shared/calendar/calendar.component.ts
@@ -13,4 +13,32 @@ export class CalendarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  skipToPreviousMonth():void{
+    this.calendarService.skipToPreviousMonth();
+  }
+  skipToNextMonth():void{
+    this.calendarService.skipToNextMonth();
+  }
+
+  getDaysOfCurrentMonth():Date[]{
+    console.log(this.calendarService.getDaysOfCurrentMonth());
+    return this.calendarService.getDaysOfCurrentMonth();
+  }
+
+  getDayNames():string[]{
+    return this.calendarService.dayNames;
+  }
+
+  getMonthFromNumber():string{
+    return this.calendarService.getMonthFromNumber(this.calendarService.currentMonth);
+  }
+  getDayOfFirstDayInMonth():number{
+    return this.calendarService.getDayOfFirstDayInMonth();
+  }
+  getPrevMonthDays():Date[]{
+    return this.calendarService.getPrevMonthDays();
+  }
+  getNextMonthDays():Date[]{
+    return this.calendarService.getNextMonthDays();
+  }
 }
--- frontend/src/app/services/calendar.service.ts
+++ frontend/src/app/services/calendar.service.ts
@@ -4,54 +4,128 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalendarService {
-  today:Date=new Date();
-  allMonthsOfYear:Date[][]=[];
-  currentYear:number=this.today.getFullYear();
-  currentMonth:number=0;
-  constructor() { 
+  today: Date = new Date();
+  allMonthsOfYear: Date[][] = [];
+  allMonthsOfPreviousYear: Date[][] = [];
+  allMonthsOfNextYear: Date[][] = [];
+  currentYear: number = this.today.getFullYear();
+  currentMonth: number =10; //this.today.getMonth()
+
+  selectedStartDay: number = -1;
+  selectedEndDay: number = -1;
+
+  dayNames:string[]=["Mo","Di","Mi","Do","Fr","Sa","So"];
+
+  constructor() {
     this.generateNewYear(this.currentYear);
     console.log(this.allMonthsOfYear);
   }
 
-  generateNewYear(year:number):void{
-    for(let i=0;i<12;i++){
-      this.allMonthsOfYear.push(this.getDaysInMonthUTC(i,year));
+  generateNewYear(year: number): void {
+    this.allMonthsOfYear=[];
+    this.allMonthsOfNextYear=[];
+    this.allMonthsOfPreviousYear=[];
+    for (let i = 0; i < 12; i++) {
+      this.allMonthsOfYear.push(this.getDaysInMonthUTC(i, year));
+      this.allMonthsOfPreviousYear.push(this.getDaysInMonthUTC(i, year-1));
+      this.allMonthsOfNextYear.push(this.getDaysInMonthUTC(i, year+1));
     }
   }
-
-   getDaysInMonth(month:number, year:number):Date[] {
-    let date:Date = new Date(year, month, 1);
-     let days:Date[] = [];
+/*
+  getDaysInMonth(month: number, year: number): Date[] {
+    let date: Date = new Date(year, month, 1);
+    let days: Date[] = [];
     while (date.getMonth() === month) {
       days.push(new Date(date));
       date.setDate(date.getDate() + 1);
     }
     return days;
   }
-
- getDaysInMonthUTC(month:number, year:number):Date[] {
-    var date:Date = new Date(Date.UTC(year, month, 1));
-    let days:Date[] = [];
+*/
+  getDaysInMonthUTC(month: number, year: number): Date[] {
+    var date: Date = new Date(Date.UTC(year, month, 1));
+    let days: Date[] = [];
     while (date.getUTCMonth() === month) {
       days.push(new Date(date));
       date.setUTCDate(date.getUTCDate() + 1);
     }
     return days;
   }
-  skipToPreviousMonth():void{
+  skipToPreviousMonth(): void {
     this.currentMonth--;
-    if(this.currentMonth<0){
-      this.currentMonth=11;
+    if (this.currentMonth < 0) {
+      this.currentMonth = 11;
       this.currentYear--;
       this.generateNewYear(this.currentYear);
     }
   }
-  skipToNextMonth():void{
+  skipToNextMonth(): void {
     this.currentMonth++;
-    if(this.currentMonth>=12){
-      this.currentMonth=0;
+    if (this.currentMonth >= 12) {
+      this.currentMonth = 0;
       this.currentYear++;
       this.generateNewYear(this.currentYear);
     }
   }
+  getDaysOfCurrentMonth():Date[]{
+    return this.allMonthsOfYear[this.currentMonth];
+  }
+  getDayOfFirstDayInMonth():number{
+    let day=this.getDaysOfCurrentMonth()[0].getDay();
+    if(day==0){
+      return 6;
+    }else{
+      return day-1;
+    }
+    //return this.getDaysOfCurrentMonth()[0].getDay();
+  }
+  getPrevMonthDays():Date[]{
+    let prevMonthDays:Date[]=[];
+    console.log(this.getDayOfFirstDayInMonth());
+    for(let i=this.getDayOfFirstDayInMonth()-1;i>=0;i--){ //todo ral for
+      if(this.currentMonth==0){
+        let prevMonth=this.allMonthsOfPreviousYear[11];
+        prevMonthDays.push(prevMonth[prevMonth.length-1-i]);
+      }else{
+        let prevMonth=this.allMonthsOfYear[this.currentMonth-1];
+        prevMonthDays.push(prevMonth[prevMonth.length-1-i]);
+      }
+    }
+    console.log(prevMonthDays)
+    return prevMonthDays;
+  }
+
+  getNextMonthDays():Date[]{
+    let nextMonthDays:Date[]=[];
+    for(let i=0;i<7-this.getDayOfFirstDayInMonth()-1;i++){ //todo ral for
+      if(this.currentMonth==11){
+        let nextMonth=this.allMonthsOfNextYear[0];
+        nextMonthDays.push(nextMonth[i]);
+      }else{
+        let nextMonth=this.allMonthsOfYear[this.currentMonth+1];
+        nextMonthDays.push(nextMonth[i]);
+      }
+    }
+    console.log(nextMonthDays)
+    return nextMonthDays;
+  }
+
+  getMonthFromNumber(ind: number): string {
+    switch (ind) {
+      case 0: return "Januar"; break;
+      case 1: return "Februar"; break;
+      case 2: return "März"; break;
+      case 3: return "April"; break;
+      case 4: return "May"; break;
+      case 5: return "Juni"; break;
+      case 6: return "Juli"; break;
+      case 7: return "August"; break;
+      case 8: return "September"; break;
+      case 9: return "Oktober"; break;
+      case 10: return "November"; break;
+      case 11: return "Dezember"; break;
+      default: return "Januar";
+    }
+  }
+
 }
