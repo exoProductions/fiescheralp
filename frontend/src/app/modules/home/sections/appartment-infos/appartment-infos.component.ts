f221ab6f16e3c672de0fe4ecfbb81d5289aac3c5
dominic gut <gut.dominic@gmx.ch>
2022-11-18 16:13:19 +0100

    home structure finished

--- frontend/src/app/modules/home/sections/appartment-infos/appartment-infos.component.ts
+++ frontend/src/app/modules/home/sections/appartment-infos/appartment-infos.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppartmentInfosComponent implements OnInit {
 
+  contents:any[]=[
+    {
+      firstLetter:"E",
+      titleOne:"ine",
+      titleTwo:"Kurze Übersicht",
+      text:"öalsdfj asdjf alsdjf asdf laksjdf kajsdf jasdöfk jaösdkfjaöskdfj asödfj asdfasdk fjöasdfjasdfas dölkfjasödfjasdfadsf adsfjöaksdfasdf asdf asdf asdfadsf asdf  ",
+    },
+    {
+      firstLetter:"V",
+      titleOne:"or Ort",
+      titleTwo:"Vorhanden",
+      text:"öalsdfj asdjf alsdjf asdf laksjdf kajsdf jasdöfk jaösdkfjaöskdfj asödfj asdfasdk fjöasdfjasdfas dölkfjasödfjasdfadsf adsfjöaksdfasdf asdf asdf asdfadsf asdf  ",
+    },
+    {
+      firstLetter:"I",
+      titleOne:"hre",
+      titleTwo:"Gastgeber",
+      text:"öalsdfj asdjf alsdjf asdf laksjdf kajsdf jasdöfk jaösdkfjaöskdfj asödfj asdfasdk fjöasdfjasdfas dölkfjasödfjasdfadsf adsfjöaksdfasdf asdf asdf asdfadsf asdf  ",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
