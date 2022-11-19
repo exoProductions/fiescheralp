1983a15a74c413ea7d7c84489c945c7a5ac35e4b
dominic gut <gut.dominic@gmx.ch>
2022-11-19 13:50:11 +0100

    nav update, book now button, start of reservations

--- frontend/src/app/modules/home/sections/appartment-infos/appartment-infos.component.ts
+++ frontend/src/app/modules/home/sections/appartment-infos/appartment-infos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appartment-infos',
@@ -28,9 +29,12 @@ export class AppartmentInfosComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  goToReservation():void{
+    this.router.navigate(["Reservation"]);
   }
 
 }
