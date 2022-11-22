7a93bb467ababeb940e738854457ec9c08bc3a95
dominic gut <gut.dominic@gmx.ch>
2022-11-22 20:52:20 +0100

    reservation page

--- frontend/src/app/modules/reservation/reservation.module.ts
+++ frontend/src/app/modules/reservation/reservation.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationComponent } from './reservation/reservation.component';
+import { SharedModule } from '../shared/shared.module';
+import { InfoSectionComponent } from './sections/info-section/info-section.component';
+import { SettingsSectionComponent } from './sections/settings-section/settings-section.component';
 
 
 
 @NgModule({
   declarations: [
-    ReservationComponent
+    ReservationComponent,
+    InfoSectionComponent,
+    SettingsSectionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class ReservationModule { }
---
+++ frontend/src/app/modules/reservation/sections/settings-section/settings-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-section',
+  templateUrl: './settings-section.component.html',
+  styleUrls: ['./settings-section.component.less']
+})
+export class SettingsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
