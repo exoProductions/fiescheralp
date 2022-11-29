f97fabebc2e20752fab807829147c459d44040f5
dominic gut <gut.dominic@gmx.ch>
2022-11-30 00:15:15 +0100

    added svg clippath to activities

--- frontend/src/app/modules/activities/activities/activities.component.ts
+++ frontend/src/app/modules/activities/activities/activities.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { NavigationService } from 'src/app/services/navigation.service';
@@ -8,24 +8,92 @@ import { NavigationService } from 'src/app/services/navigation.service';
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.less']
 })
-export class ActivitiesComponent implements OnInit,OnDestroy {
-
+export class ActivitiesComponent implements OnInit, OnDestroy, AfterViewInit {
   private routeSub!: Subscription;
-  activityInd:number=0;
-  constructor(private navigationService:NavigationService,private route:ActivatedRoute) { 
+  activityInd: number = 0;
+  @ViewChild("path") pathElem!: ElementRef;
+
+  activityRows: any[][] = [
+    [
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 0,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 1,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 2,
+      },
+    ],
+    [
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 3,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 4,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 5,
+      },
+    ],
+    [
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 6,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 7,
+      },
+      {
+        activityTitle: "Activity one",
+        shortDescription: "Short Description of Activity one",
+        activityInd: 8,
+      },
+    ],
+  ]
+  pathLength:number=10;
+
+  constructor(private navigationService: NavigationService, private route: ActivatedRoute) {
     this.routeSub = this.route.params.subscribe(params => {
       console.log(params['id']);
       this.activityInd = params['id'];
     });
   }
 
+
   ngOnInit(): void {
-    this.navigationService.curPageInd=3;
+    this.navigationService.curPageInd = 3;
     this.gotoTop();
   }
-
+  ngAfterViewInit(): void {
+ //this.pathLength  = this.pathElem.nativeElement.getTotalLength();
+    this.pathElem.nativeElement.style.strokeDasharray = this.pathLength + ' ' + this.pathLength;
+    this.pathElem.nativeElement.style.strokeDashoffset = this.pathLength;
+  }
   ngOnDestroy(): void {
     this.routeSub.unsubscribe();
+  }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    let scrollPercentage=(document.documentElement.scrollTop + document.body.scrollTop)/(document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    let drawLength=this.pathLength*scrollPercentage;
+    this.pathElem.nativeElement.style.strokeDashoffset=this.pathLength-drawLength;
   }
 
   gotoTop() {
