ee82596793df41cb6ea5e2dda327fbb7370ad08b
dominic gut <gut.dominic@gmx.ch>
2022-12-01 17:45:46 +0100

    activities update style and list view content

--- frontend/src/app/modules/activities/activities/activity-data.ts
+++ frontend/src/app/modules/activities/activities/activity-data.ts
@@ -1,36 +1,36 @@
 export class ActivityData {
     activities: any[] = [
         {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
+            activityTitle: "Fiesch bei Nacht",
+            shortDescription: "Das Restaurant Bergfreund bietet eine gemütliche Atmosphäre für ein Dinner bei prasselndem Kaminfeuer.",
             activityInd: 0,
             subclass: "activity-one"
         },
         {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
-            activityInd: 0,
-            subclass: "activity-one"
+            activityTitle: "Alpenlodge Kühboden",
+            shortDescription: "Nebst bodenständigen Verwöhnungen aus der Hausküche bietet das Kühboden auch eine phantastische Aussicht.",
+            activityInd: 1,
+            subclass: "activity-two"
         }, {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
-            activityInd: 0,
-            subclass: "activity-one"
+            activityTitle: "Heidis Hütte",
+            shortDescription: "Für alle die es etwas edler mögen: Heidis Hütte - Der kulinarische Hotspot der Aletsch-Arena",
+            activityInd: 2,
+            subclass: "activity-three"
         }, {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
-            activityInd: 0,
-            subclass: "activity-one"
+            activityTitle: "Ski fahren",
+            shortDescription: "Im Winter bietet die Aletsch-Arena die perfekten voraussetzungen für Ihren Ski Urlaub. Tickets ab 50.- auf Aletscharena.ch",
+            activityInd: 3,
+            subclass: "activity-four"
         }, {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
-            activityInd: 0,
-            subclass: "activity-one"
+            activityTitle: "Wandern",
+            shortDescription: "Spektakuläre Gräte, tiefblaue Bergseen, duftende Arvenwälder - Entdecke die Aletsch-Arena zu Fuss",
+            activityInd: 4,
+            subclass: "activity-five"
         }, {
-            activityTitle: "Activity one",
-            shortDescription: "Short Description of Activity one",
-            activityInd: 0,
-            subclass: "activity-one"
+            activityTitle: "Weitere Aktivitäten",
+            shortDescription: "Nicht nur auf den Skiern, sondern auch mit Schneeschuhen und aus der Luft per Gleitschirm kann das Gebiet erkundet werden (Aletscharena.ch). ",
+            activityInd: 5,
+            subclass: "activity-six"
         },
     ]
 }
